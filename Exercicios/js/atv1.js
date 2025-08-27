deff963b4605f4df7e456917f00c4aabbcb06f23
anaclaramachado1234 <anaclaramachado.ifpr.2023@gmail.com>
2025-08-27 16:11:09 -0300

    2708 atv 1
    Atv1= concluída.

---
+++ Exercicios/js/atv1.js
@@ -0,0 +1,9 @@
+var n , r
+n=parseInt(prompt("Digite um valor:"))
+if(n<=3){
+    r=n*3
+    alert("Seu número é maenor ou igual a 3, o triplo dele é: "+r)
+}
+else(
+    alert("Seu número é maior que três.")
+)
