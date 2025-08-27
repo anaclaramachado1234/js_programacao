61c53df0e8a55fcfaed0e316d9bc37b88ded9d51
anaclaramachado1234 <anaclaramachado.ifpr.2023@gmail.com>
2025-08-27 16:17:18 -0300

    atualizando atvd 2
    Concluída a atvd 2, faltando teste.

---
+++ Exercicios/js/atv2.js
@@ -0,0 +1,12 @@
+// Refaça o código anterior e numero seja menor ou igual a 3 apresente o dobro. Caso o numero seja maior apresente o triplo
+var n , r , t
+n=parseInt(prompt("Digite um valor:"))
+if(n<=3){
+    r=n*2
+    alert("Seu número é maenor ou igual a 3, o dobro dele é: "+r)
+}
+else{
+    t=n*3
+    alert("Seu número é maior que três. Portanto aqui esta o triplo: "+t)
+}
+    
